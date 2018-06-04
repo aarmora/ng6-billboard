d3f964d7ec132888157bc5494489fc0c74f8b04d
Jordan Hansen <jbhansen84@gmail.com>
2018-06-04 03:41:36 -0600

    Initial commit, have navbar, need more dummy content

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.7.
 
+Note that the versions of the dependencies (found in package.json) are locked at their current versions. If you desire to update them, you will have to update them manually.
+
+## Banner Images
+Banner images come from [Unsplash](https://unsplash.com). They are currently choosen randomly. You can use your own and replace the background-image in `/src/app/components/app.component.css`.
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+import { MatMenuModule, MatToolbarModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
