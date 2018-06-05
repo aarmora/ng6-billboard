5b3ff41f4724bfbf64b6fa5f2b3d40e57e89682c
Jordan Hansen <jbhansen84@gmail.com>
2018-06-05 06:19:00 -0600

    Tests are passing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,29 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material';
+import { AppRoutingModule } from './app-routing.module';
+import { APP_BASE_HREF } from '@angular/common';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NavbarComponent,
+        HomeComponent,
+        AboutUsComponent
       ],
+      imports: [
+        RouterModule,
+        MatMenuModule,
+        AppRoutingModule
+      ],
+      providers: [
+        {provide: APP_BASE_HREF, useValue: '/'}
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -17,11 +35,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to billboardApp!');
   }));
 });
--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import { MatMenuModule, MatButtonModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from '../../app-routing.module';
+import { HomeComponent } from '../home/home.component';
+import { AboutUsComponent } from '../about-us/about-us.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -8,7 +14,20 @@ describe('NavbarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
+      declarations: [ 
+        NavbarComponent,
+        HomeComponent,
+        AboutUsComponent 
+      ],
+      imports: [
+        MatMenuModule,
+        RouterModule,
+        MatButtonModule,
+        AppRoutingModule
+      ],
+      providers: [
+        {provide: APP_BASE_HREF, useValue: '/'}
+      ]
     })
     .compileComponents();
   }));
